ea9450992333f0e06fce6d4ff99d30a392b03b5f
Amaar <avallian@ucsd.edu>
2022-11-21 14:22:53 -0800

    explore

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # Lab 8 - Starter
-1. Within a GitHub Action that runs whenever the code is pushed because ... 
+## Amaar Valliani
+
+![Screenshot 1](screenshots/1.png)
+![Screenshot 2](screenshots/2.png)
+![Screenshot 3](screenshots/3.png)
+![Screenshot 4](screenshots/4.png)
+
+1. Within a GitHub Action that runs whenever the code is pushed because that way when someone pushes a new feature, you can test the code to make sure nothing is broken and this will happen automatically throught GitHub Actions when you push.
+2. No
+3. No because the message feature will have many other features it interacts with and has many moving parts for it to work so it would be more suitible to use an e2e test for the feature rather than a unit test
+4. Yes because this feature is does not depend on other features or functions and should be confined to a few or one function so it will be easy to test it using unit tests
--- __tests__/unit.test.js
+++ __tests__/unit.test.js
@@ -3,3 +3,83 @@
 const functions = require('../code-to-unit-test/unit-test-me.js');
 
 // TODO - Part 2
+
+test('Check 281-998-3241 is phone number', () => {
+    expect(functions.isPhoneNumber('281-998-3241')).toBe(true);
+});
+
+test('Check 999-9999-9999 is phone number', () => {
+    expect(functions.isPhoneNumber('999-9999-9999')).toBe(true);
+});
+
+test('Check 505 is not phone number', () => {
+    expect(functions.isPhoneNumber('505')).toBe(false);
+});
+
+test('Check 2819983241 is not phone number', () => {
+    expect(functions.isPhoneNumber('2819983241')).toBe(false);
+});
+
+test('Check avallian@ucsd.edu is email', () => {
+    expect(functions.isEmail('avallian@ucsd.edu')).toBe(true);
+});
+
+test('Check chicken0fried@steak.co is email', () => {
+    expect(functions.isEmail('chicken0fried@steak.co')).toBe(true);
+});
+
+test('Check avallian@ucsd,edu is not email', () => {
+    expect(functions.isEmail('avallian@ucsd,edu')).toBe(false);
+});
+
+test('Check chickenfried.steak@com is not email', () => {
+    expect(functions.isEmail('chickenfried.steak@com')).toBe(false);
+});
+
+test('Check StrongPassword is strong password',() => {
+    expect(functions.isStrongPassword('StrongPassword')).toBe(true);
+});
+
+test('Check a123 is strong password',() => {
+    expect(functions.isStrongPassword('a123')).toBe(true);
+});
+
+test('Check a12 is not strong password',() => {
+    expect(functions.isStrongPassword('a12')).toBe(false);
+});
+
+test('Check NotStrongPassword is not strong password',() => {
+    expect(functions.isStrongPassword('NotStrongPassword')).toBe(false);
+});
+
+test('Check 11/21/2022 is date', () => {
+    expect(functions.isDate('11/21/2022')).toBe(true);
+});
+
+test('Check 1/21/2022 is date', () => {
+    expect(functions.isDate('1/21/2022')).toBe(true);
+});
+
+test('Check 11-21-2022 is not date', () => {
+    expect(functions.isDate('11-21-2022')).toBe(false);
+});
+
+test('Check 11/21/22 is not date', () => {
+    expect(functions.isDate('11/21/22')).toBe(false);
+});
+
+test('Check FFFFFF is hex code', () => {
+    expect(functions.isHexColor('FFFFFF')).toBe(true);
+});
+
+test('Check ABC is hex code', () => {
+    expect(functions.isHexColor('ABC')).toBe(true);
+});
+
+test('Check FFFFFFFFF is not hex code', () => {
+    expect(functions.isHexColor('FFFFFFFFF')).toBe(false);
+});
+
+test('Check EFG is not hex code', () => {
+    expect(functions.isHexColor('EFG')).toBe(false);
+});
